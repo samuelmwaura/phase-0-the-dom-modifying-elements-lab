269f0ae82d488d3e3a8f2298cadf4b9924befa3c
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-04 13:13:00 +0300

    commit tree 9a54a588d2ed7886a22bfd462d5190809a108fa1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+document.body.innerHTML= "<h1 id='victory'></h1>";
+const newHeader = document.getElementById('victory');
+newHeader.textContent = 'Mwaura is the champion';
